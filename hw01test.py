871c0188c9812176cb33cbbe77d5d59cdaf67aae
kavyajadav <63379421+kavyajadav@users.noreply.github.com>
2021-09-26 01:34:55 -0400

    Add files via upload

---
+++ hw01test.py
@@ -0,0 +1,72 @@
+import unittest
+
+
+def classify_triangle(a, b, c):
+    """
+    Method that Classifies the triangle based on the sides given
+    """
+    intersection = {a, b, c} & {a, b, c}
+    is_right_triangle = a ** 2 + b ** 2 == c ** 2
+    triangle_class = 'Not a Triangle'
+
+    if a <= 0 or b <= 0 or c <= 0:
+        return triangle_class
+
+    if is_right_triangle:
+        triangle_classification = 'Right Angle Triangle'
+    elif len(intersection) == 1:
+        triangle_classification = 'Equilateral  Triangle'
+    elif len(intersection) == 2:
+        triangle_classification = 'Isosceles Triangle'
+    else:
+        triangle_classification = 'Scalene Triangle'
+
+    return triangle_classification
+
+
+class TriangleClassification(unittest.TestCase):
+    """
+    This function returns a string with the type of triangle from three  values
+    corresponding to the lengths of the three sides of the Triangle.
+    """
+
+    def test_classify_equilateral_triangles(self):
+        """
+        Testcases for equilateral triangle
+        """
+        self.assertEqual(classify_triangle(1, 1, 1), 'Equilateral  Triangle')
+        self.assertEqual(classify_triangle(10, 10, 10), 'Equilateral  Triangle')
+
+    def test_classify_isosceles_triangles(self):
+        """
+        Testcases classify isosceles triangle
+        """
+        self.assertEqual(classify_triangle(65, 65, 130), 'Isosceles Triangle')
+        self.assertEqual(classify_triangle(2, 3, 3), 'Isosceles Triangle')
+        self.assertEqual(classify_triangle(4, 6, 4), 'Isosceles Triangle')
+
+    def test_classify_scalene_triangles(self):
+        """
+        Testcases scalene triangle
+        """
+        self.assertEqual(classify_triangle(1, 2, 3), 'Scalene Triangle')
+        self.assertEqual(classify_triangle(7, 6, 3), 'Scalene Triangle')
+        self.assertEqual(classify_triangle(67, 87, 90), 'Scalene Triangle')
+
+    def test_classify_right_triangles(self):
+        """
+        Testcases classify right triangle
+        """
+        self.assertEqual(classify_triangle(3, 4, 5), 'Right Angle Triangle')
+        self.assertEqual(classify_triangle(5, 12, 13), 'Right Angle Triangle')
+        self.assertEqual(classify_triangle(8, 15, 17), 'Right Angle Triangle')
+
+    def test_classify_not_a_triangle(self):
+        """
+        Testcases classify
+        """
+        self.assertEqual(classify_triangle(-10, -10, -10), 'Not a Triangle')
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
