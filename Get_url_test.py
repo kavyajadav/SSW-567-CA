32eb01ad59603f87c345d9e968da8bb34831be90
kavyajadav <63379421+kavyajadav@users.noreply.github.com>
2021-12-08 14:36:25 -0500

    Create Get_url_test.py

---
+++ Get_url_test.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest import mock
+from Get_url import get_repo_info
+
+
+class TestGetRepo(unittest.TestCase):
+    def test_normal_response(self):
+        """Testing to check for expected repositories"""
+        expected = [
+            "User: Kavya567",
+            "Repo: Django Number of commits: 1",
+            "Repo: embedded-projects Number of commits: 2",
+            "Repo: helloworld Number of commits: 1",
+            "Repo: Image-Processing Number of commits: 2",
+            "Repo: IoT-automation Number of commits: 2",
+            "Repo: Python_Repository Number of commits: 6",
+            "Repo: testing Number of commits: 30",
+            "Repo: Web-controlled-robot Number of commits: 2",
+            "Repo: web-_programming Number of commits: 2",
+        ]
+        self.assertEqual(get_repo_info(), expected)
+
+    # def test_bad_user_name(self):
+    #     self.assertEqual(get_repo_info(), 'unable to fetch repos from user')
+
+
+if __name__ == "__main__":
+    unittest.main(exit=False, verbosity=2)
